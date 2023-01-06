437c035ea5bd8c93b54348772eff618878ec3a74
abdoulfataoh <abdoulfataoh@gmail.com>
2023-01-06 16:02:29 +0000

    remove choose_task_handler and choose_deposit_operator_handler handlers

--- app/telegram/handlers.py
+++ app/telegram/handlers.py
@@ -14,19 +14,3 @@ def hello_handler(app: Application) -> None:
     )
 
 
-def choose_task_handler(app: Application) -> None:
-    app.add_handler(
-        CommandHandler(
-            command='task',
-            callback=callbacks.choose_task
-        )
-    )
-
-
-def choose_deposit_operator_handler(app: Application) -> None:
-    app.add_handler(
-        CommandHandler(
-            command='operator',
-            callback=callbacks.choose_deposit_operator
-        )
-    )
